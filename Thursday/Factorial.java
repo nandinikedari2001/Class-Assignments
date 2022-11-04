c6fcbb3fcb0671ba7c99d9cc6f3fd9c1dab680ab
Nandini <87691034+nandinikedari2001@users.noreply.github.com>
2022-11-04 13:21:22 +0530

    Create Factorial.java

---
+++ Thursday/Factorial.java
@@ -0,0 +1,26 @@
+package basicpackage;
+
+import java.util.Scanner;
+
+public class Factorial {
+	public static void main(String[] args)
+	{
+		int n,fact=1,i;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter number: ");
+		n = sc.nextInt();
+		for(i=1;i<=n;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println("Factorial of "+n+" is "+fact);
+		sc.close();
+	}
+}
+
+
+/*
+OUTPUT:
+Enter number: 5
+Factorial of 5 is 120
+*/
